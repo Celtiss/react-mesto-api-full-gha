98e7dfb6930c81a0a5c27695088b7d11d1c87929
Elena Temirbekova <temn.lena@mail.ru>
2023-04-14 19:10:38 +0300

    feat: Добавить frontend на ВМ

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,23 @@ mongoose.connect(DB_PATH, {
   .then(() => console.log('connected'))
   .catch((err) => console.log(`Ошибка подключения базы данных: ${err}`));
 
+// Обработка CORS
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+
+app.use(function(req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
---
+++ frontend/src/components/EditAvatarPopup.js
@@ -0,0 +1,34 @@
+import React, { useRef, useEffect } from 'react';
+import PopupWithForm from './PopupWithForm';
+
+function EditAvatarPopup({isOpen, onClose, onUpdateAvatar, handleEscClose}) {
+    const avatar = useRef();
+
+    useEffect(() => {
+        avatar.current.value = '';
+      }, [isOpen]); 
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        onUpdateAvatar({
+          avatar: avatar.current.value
+        });
+      }
+    return (
+        <PopupWithForm
+            title='Обновить аватар'
+            name='edit-avatar'
+            btnName='Сохранить'
+            isOpen={isOpen}
+            onClose={onClose}
+            onSubmit={handleSubmit}
+            handleEscClose={handleEscClose}>
+            <label className="popup__form-label">
+                <input name="popupAvatar" type="url" ref={avatar} placeholder="Ссылка на аватарку" id="avatar-input" className="popup__input popup__input_value_avatar" required />
+                <span className="popup__input-error avatar-input-error"></span>
+            </label>
+        </PopupWithForm>
+    )
+}
+
+export default EditAvatarPopup;
