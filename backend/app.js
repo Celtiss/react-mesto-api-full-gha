403964ac1c1b77dd80d22a02495cfb5ac8951894
Elena Temirbekova <temn.lena@mail.ru>
2023-04-14 20:59:27 +0300

    feat: Добавить обработку CORS

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
