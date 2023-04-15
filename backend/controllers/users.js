55986eb82f12a1e05d4e4d8b106f9a4ec305fa69
Elena Temirbekova <temn.lena@mail.ru>
2023-04-15 22:24:18 +0300

    fix: Добавить cookie

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -14,8 +14,8 @@ module.exports.login = (req, res, next) => {
       res.cookie('token', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        secure: true,
-        sameSite: 'None',
+        // secure: true,
+        // sameSite: 'None',
       }).send({ user })
         .end();
     })
