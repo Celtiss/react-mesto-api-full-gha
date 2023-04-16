85d304c4ff69bdc2f04031e05626b814b96adf57
Elena Temirbekova <temn.lena@mail.ru>
2023-04-16 21:13:49 +0300

    fix: Исправить лайки

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,7 +4,7 @@ import { CurrentUserContext} from '../contexts/CurrentUserContext.js';
 function Card ({card, onCardClick, onCardLike, onCardDelete}) {
     const currentUser = React.useContext(CurrentUserContext);
     const isOwn = card.owner._id === currentUser._id;
-    const isLiked = card.likes.some(i => i === currentUser._id);
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
     // Создаём переменную, которую после зададим в `className` для кнопки лайка
     const cardLikeButtonClassName = ( 
         `elements__like-icon ${isLiked && 'elements__like-icon_active'}` 
