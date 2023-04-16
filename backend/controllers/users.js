179277d1340b2143154cfb26b68716b6ffbc9913
Elena Temirbekova <temn.lena@mail.ru>
2023-04-16 17:29:15 +0300

    fix: Сделать рефакторинг

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -15,7 +15,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       if (NODE_ENV === 'production') {
-        res.cookie('token', token, {
+        res.cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           secure: true,
@@ -23,7 +23,7 @@ module.exports.login = (req, res, next) => {
         }).send({ user })
           .end();
       } else {
-        res.cookie('token', token, {
+        res.cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
         }).send({ user })
