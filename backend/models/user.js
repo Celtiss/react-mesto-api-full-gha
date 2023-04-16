591e8373a21a6e6f07c40e4b92312184bdd89697
Elena Temirbekova <temn.lena@mail.ru>
2023-04-16 19:58:09 +0300

    fix: Исправить схему mongoose

--- backend/models/user.js
+++ backend/models/user.js
@@ -31,6 +31,7 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
 });
 
@@ -42,12 +43,11 @@ userSchema.path('email').validate((val) => {
 }, 'Invalid email.');
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
       }
-      console.log(password, user.password);
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
